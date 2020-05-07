cb9e2a5db52976c6a587ff731b343451a29bdc03
Damian Cyrana <damiancyrana@gmail.com>
2020-05-07 19:03:13 +0200

    Implemented a system of post recommendations

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from .forms import CommentForm
 from .models import Post
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -24,7 +25,8 @@ def post_list(request, tag_slug=None):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'blog/table_post.html', {'page': page, 'posts': posts, 'tag': tag})
+    context = {'page': page, 'posts': posts, 'tag': tag}
+    return render(request, 'blog/table_post.html', context)
 
 
 def specific_post(request, year, month, day, post):
@@ -45,5 +47,9 @@ def specific_post(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
-    context = {'post': post, 'comments': comments, 'comment_form': comment_form}
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar = similar.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:5]
+
+    context = {'post': post, 'comments': comments, 'comment_form': comment_form, 'similar': similar}
     return render(request, 'blog/specific_post.html', context)
